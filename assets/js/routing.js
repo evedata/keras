f590b3d6826d35164305d633624d249746fd3e98
evedata <266322@qip.ru>
2018-03-17 22:22:15 +0300

    okoko

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -1,4 +1,1 @@
-fos.Router.setData({"base_url":"","routes":{"megalo_salomon_get_popinonsite_data":{"tokens":[["text","\/popinonsite\/renderPopin"],
-["variable","\/","[^\/]++","locale"]],"defaults":[],"requirements":{"_scheme":"https"},"hosttokens":[]},
-"megalo_salomon_mesh_unique_to_me_popin_authorisation":{"tokens":[["text","\/Slabmesh\/unique-to-me-popin-authorisation"],
-["variable","\/","[^\/]++","locale"]],"defaults":[],"requirements":{"_scheme":"https"},"hosttokens":[]}},"prefix":"","host":"www.salomon.com","scheme":"https"});
+fos.Router.setData({"base_url":"","routes":{"megalo_salomon_get_popinonsite_data":{"tokens":[["text","\/popinonsite\/renderPopin"],["variable","\/","[^\/]++","locale"]],"defaults":[],"requirements":{"_scheme":"https"},"hosttokens":[]},"megalo_salomon_mesh_unique_to_me_popin_authorisation":{"tokens":[["text","\/Slabmesh\/unique-to-me-popin-authorisation"],["variable","\/","[^\/]++","locale"]],"defaults":[],"requirements":{"_scheme":"https"},"hosttokens":[]}},"prefix":"","host":"www.salomon.com","scheme":"https"});
